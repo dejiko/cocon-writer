8a1ecbf19521e8a894487c923a9f1280246d2b4b
SHIMADA Hirofumi <shimada@c1075>
2017-03-06 01:28:02 +0900

    just works it

--- cocon-writer.sh
+++ cocon-writer.sh
@@ -1,18 +1,22 @@
-#!/bin/bash
+#/bin/bash
 
 CDDRV="/dev/sr0"
 
-# 
+# Wait inserted CD-R
 wait_insert()
 {
 	eject $CDDRV
+	echo "PLEASE INSERT BLANK CD."
 
 	# Wait inserted
 	while :
 	do
-		cdstat=$( cdinfo $CDDRV 2>&1 | grep -q "no_disc" || echo "1" )
-		if [ $cdstat ];
+		cur="$( sg_get_config $CDDRV 2>&1 | grep "Current profile:" )"
+		if [ -n "$cur" ];
 		then
+			# Check profile
+			cdprof=$( echo $cur | grep "Current profile: " | cut -d ":" -f 2 | tr -d ' ' )
+			echo "TYPE: $cdprof"
 			break
 		fi
 		sleep 1
@@ -26,41 +30,84 @@ wait_insert()
 # Erase CD-RW.
 rw_erase()
 {
-	wodim dev=$CDDRV blank=fast
+	wodim -v dev=$CDDRV blank=fast
 	eject $CDDRV
 }
 
 # Write disk image
 writeimg()
 {
-	if [ -e $1 ];
+	if [ -e $1 -a -n "$2" ];
 	then
 		iso="$1"
+		iso_md5="$2"
 	else
 		echo "ERR: FILE $1 NOT FOUND!"
 		exit 1
 	fi
 
 	# Write image
-	wodim dev=$CDDEV $1
+	wodim -v -dao dev=$CDDRV -data $iso
+	echo "WRITE COMPLETE (RETURN: $?)."
 
 	# Temporary eject and insert
-	sdparm --command=unlock $CDDEV
-	sg_start -e -i $CDDEV
-	sg_start -l -i $CDDEV
-
+	sdparm --command=unlock $CDDRV
+	sg_start -e -i $CDDRV
+	sg_start -l -i $CDDRV
 
 	# Verify
+	while :
+	do
+		cd_md5all=$( md5sum $CDDRV 2> /dev/null )
+		cd_md5stat=$?
 
+		if [ $cd_md5stat -ne 0 ];
+		then
+			# I/O Error. retry.
+			sleep 1
+			continue
+		fi
+		cd_md5=$( echo $cd_md5all | cut -d " " -f 1 )
 
-	sleep 1
+		if [ "$iso_md5" != "$cd_md5" ];
+		then
+			# Inccorect checksum.
+			echo "ERR: Incorrect MD5!"
+			echo " ISO $iso_md5"
+			echo " CDR $cd_md5"
+			sleep 10
+		fi
+		break
+	done
+
+	# eject cd
+	eject $CDDRV
+
+	#
+	echo " WRITE OK."
+	sleep 2
 }
 
 # Verify MD5 on start
+find "./" -type f -iname "?.imagedef" | sort | while read -r defname
+do
+	defnum=$( basename $defname .imagedef )
+	source $defname
+
+	if [ -n "$IMG_ISOFILE" ];
+	then
+		chk_md5=$( md5sum $IMG_ISOFILE | cut -d " " -f 1 )
+		if [ "$IMG_MD5" != "$chk_md5" ];
+		then
+			echo "$IMG_ISOFILE : MD5 incorrect."
+			exit 1
+		fi
+		echo "$IMG_NAME ($IMG_ISOFILE) OK."
+	fi
+done
 
 
-# Main Loop
-
+# Main menu
 while :
 do
 	clear
@@ -95,7 +142,7 @@ do
 		then
 			rw_erase
 		else
-			writeimg $IMG_ISOFILE 
+			writeimg $IMG_ISOFILE $IMG_MD5
 		fi
 	else
 		echo ""
